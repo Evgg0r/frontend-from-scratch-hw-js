2df61b1b5f997a63eda99e2ea9c9c88a5e6a00d9
evgg0r <gor.evg2100@gmail.com>
2024-12-01 17:28:14 +0700

    completing task 4-3

--- lesson-04/03.js
+++ lesson-04/03.js
@@ -19,8 +19,8 @@ findCommonElements([1, 2, 3], [2, 3, 4]) // [2, 3]
 
 function findCommonElements(array1, array2) {
     let newArray = [];
-    for (let i = 0; i < array.length; i++) {
-        if (includesElement(array1,array2[i]) !== false) {
+    for (let i = 0; i < array2.length; i++) {
+        if (includesElement(array1, array2[i]) !== false) {
             newArray.push(array2[i])
         }
     }
