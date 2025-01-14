8bb5cac62905912f266caba54c624355490a4cc7
evgg0r <gor.evg2100@gmail.com>
2025-01-14 18:28:02 +0700

    task 9-2

--- lesson-09/02.js
+++ lesson-09/02.js
@@ -22,7 +22,7 @@ const map = (array, callback) => {
 
   for (let i = 0; i < array.length; i++) {
 
-    const result = callback(array[i]);
+    const result = callback(array[i], i);
 
     resultArray.push(result);
   }
